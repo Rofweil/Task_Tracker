e2d58683238e5dbeb9d8c199ec9833275f3951a4
TestProject <Rofweilveilmin@gmail.com>
2025-12-03 15:02:45 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// simple file to show page is loaded - optional
-document.addEventListener('DOMContentLoaded', () => {
-  // nothing needed yet; kept for future enhancements
-  console.log('MT Task Tracker homepage loaded');
-});
