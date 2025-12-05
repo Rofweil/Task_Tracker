684b2bc1cb9ec15a8817e76c85922242aaad8c0a
TestProject <Rofweilveilmin@gmail.com>
2025-12-05 13:11:34 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
-    base: '/Task_Tracker/',
+    base: '/BackendStaff/',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
